a0a8a45b6b8d15ce66f7b3d921dc605832082ee7
boyiechen <boyie@uchicago.edu>
2023-05-02 03:51:18 +0000

    add photos

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,21 @@
+class PhotosController < ApplicationController
+  def index
+    matching_photos = Photo.all
+    @list_of_photos = matching_photos.order({ :created_at => :desc })
+    render({ :template => "photo_templates/index.html.erb" })
+  end
+
+  def show
+    url_username = params.fetch("path_username")
+
+    matching_usernames = User.where({ :username => url_username })
+
+    @the_user = matching_usernames.first
+
+    if @the_user == nil
+      redirect_to("/404")
+    else
+      render({ :template => "user_templates/show.html.erb" })
+    end
+  end
+end
